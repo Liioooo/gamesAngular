39a44d1352ad8fc1b7a5942835ef556aaf36a67e
Liiioooo <preissegger.leo@a1.net>
2018-10-21 16:39:35 +0200

    logout gui 2

---
+++ src/app/app-manage-account/app-manage-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-account',
+  templateUrl: './app-manage-account.component.html',
+  styleUrls: ['./app-manage-account.component.css']
+})
+export class AppManageAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
