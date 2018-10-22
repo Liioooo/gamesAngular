1e8f474c2b7c23a928f0e67441e2c38e25de743a
Leo Preissegger <6155@htl.rennweg.at>
2018-10-22 11:58:22 +0200

    added p5js

--- src/app/app-falling-blocks/app-falling-blocks.component.ts
+++ src/app/app-falling-blocks/app-falling-blocks.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+import * as p5 from 'p5';
 
 @Component({
   selector: 'app-falling-blocks',
   templateUrl: './app-falling-blocks.component.html',
   styleUrls: ['./app-falling-blocks.component.css']
 })
-export class AppFallingBlocksComponent implements OnInit {
+export class AppFallingBlocksComponent implements OnInit, OnDestroy {
+
+  private p5;
 
   constructor() { }
 
   ngOnInit() {
+      this.createCanvas();
   }
+
+  ngOnDestroy() {
+      this.destroyCanvas();
+  }
+
+  private createCanvas = () => {
+    this.p5 = new p5(this.drawing);
+  };
+
+  private destroyCanvas = () => {
+    this.p5.noCanvas();
+  };
+
+  private drawing = function (p: any) {
+    p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight).parent('falling-block-canvas');
+        p.frameRate(60);
+    };
+
+    p.draw = () => {
+      p.background(255);
+      p.fill(0);
+      p.rect(p.mouseX, p.mouseY, 100, 100);
+    };
+
+  }
+
 
 }
